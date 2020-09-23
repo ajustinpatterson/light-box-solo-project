9b3802b328b4892efef4d3ffc42c233c4658229e
Justin Patterson <PJ943@yahoo.com>
2020-09-23 19:34:05 +0200

    feat(components folder): add core components
    add Login, List, Explore, Upload, Profile, Register, Curate components

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import Explore from './components/Explore';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>I have birthed!</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={[styles.gray, styles.container]}>
+        <Text style={{ color: 'lightgray' }}>I have birthed!</Text>
       </View>
+      <Explore />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  gray: { backgroundColor: 'lightgray' },
+  container: {
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
 
 export default App;
---
+++ components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Explore = () => {
+  return (
+    <View>
+      <Text>I have also birthed!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Explore;
