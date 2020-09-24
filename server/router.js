6e46e787e5a5aff12b17b579075f73c9c223283b
Justin Patterson <PJ943@yahoo.com>
2020-09-24 18:59:32 +0200

    feat(derver): add uppload route

--- server/router.js
+++ server/router.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 
 const mediactrl = require('./controllers/mediacntrl');
+const uploadFile = require('./middlewares/upload');
 const upload = require('./middlewares/upload');
+
+router.get('/', () => {});
+
+router.post('/upload', upload.single('file'), mediactrl.uploadFiles);
 
 module.exports = router;
