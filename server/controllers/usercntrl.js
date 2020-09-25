1ae1f1cbc593d17ae564c1a5a76f44d1b2a9775f
Justin Patterson <PJ943@yahoo.com>
2020-09-25 18:39:52 +0200

    feat(server): add login route and controller

--- server/controllers/usercntrl.js
+++ server/controllers/usercntrl.js
@@ -1,4 +1,5 @@
 const db = require('../models/index');
+const { use } = require('../router');
 
 exports.getAllUsers = async (req, res) => {
   try {
@@ -7,5 +8,24 @@ exports.getAllUsers = async (req, res) => {
     res.sendStatus(200);
   } catch (err) {
     console.log(err);
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await db.Users.findOne({
+      where: { username: req.body.username },
+    });
+    console.log(user);
+    user
+      ? res.status(200).send({
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          pumpkins: user.pumpkins,
+        })
+      : res.status(404).send('User not found.');
+  } catch (err) {
+    res.status(500).send(err);
   }
 };
--- server/router.js
+++ server/router.js
@@ -2,23 +2,24 @@ const router = require('express').Router();
 
 const mediaUpload = require('./controllers/mediauploadctrl');
 const userctrl = require('./controllers/usercntrl');
+
 const uploadFile = require('./middlewares/upload');
-
-router.get('/', () => {});
-
-router.get('/welcome', () => {});
 
 router.get('/users', userctrl.getAllUsers);
 
 // router.get('/create', () => {}); 3
 
-// router.get('/login', () => {}); 2
+router.get('/login', userctrl.login);
 
 // router.get('/feed', () => {});
 
 // router.get('/profile', () => {}); 2
 
 // router.get('/explore', () => {});
+
+router.post('/comment');
+
+router.post('/like');
 
 //implement later
 //router.get('/collections', () => {});
