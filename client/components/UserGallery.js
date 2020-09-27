dac195abd73b69dd08ab04dc514943e74e947bc0
Justin Patterson <PJ943@yahoo.com>
2020-09-27 13:39:01 +0200

    refactor(client): refactor app.js and usergallery to not use ImageService
    got to sleep, ImageService, you're drunk

--- client/components/UserGallery.js
+++ client/components/UserGallery.js
@@ -1,15 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
-import ImageService from '../services/ImageService';
 
 const UserGallery = () => {
   const [images, setImages] = useState([]);
 
-  const getAllImages = ImageService.getAllImages;
-
-  useEffect(() => {
-    getAllImages();
-  }, []);
   return <View></View>;
 };
 
