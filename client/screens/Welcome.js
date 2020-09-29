616fb814da3f972efe11f2beb6a3c1fcead3fe7a
Justin Patterson <PJ943@yahoo.com>
2020-09-29 15:48:56 +0200

    refactor(client): redo welcome screen with photo/login

--- client/screens/Welcome.js
+++ client/screens/Welcome.js
@@ -1,84 +1,88 @@
-import { coverage } from 'browserslist';
 import React from 'react';
 import {
-  View,
-  Text,
-  Button,
-  TouchableOpacity,
-  StyleSheet,
   ImageBackground,
-  StatusBar,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
 } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import Login from '../screens/Login';
-import Register from '../screens/Register';
-import Upload from './Upload';
 
-const image = {
-  uri:
-    'https://images.unsplash.com/photo-1549197669-65ad15308853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
-};
-
-const Welcome = ({ navigation }) => {
+const Welcome = ({}) => {
+  const bkgrndImage = {
+    uri:
+      'https://images.unsplash.com/photo-1567581807005-1a3609c32d95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  };
   return (
-    <View>
-      <StatusBar hidden />
-      <ImageBackground source={image} style={styles.backgroundImage}>
-        <TouchableOpacity
-          style={styles.appButton}
-          onPress={() => {
-            navigation.navigate('Login');
-          }}
-        >
-          <Text style={styles.buttonText}>log in</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.appButton}
-          onPress={() => {
-            navigation.navigate('Register');
-          }}
-        >
-          <Text style={styles.buttonText}>register</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.appButton}
-          onPress={() => {
-            navigation.navigate('UserGallery');
-          }}
-        >
-          <Text style={styles.buttonText}>feed</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.appButton}
-          onPress={() => {
-            navigation.navigate('Upload');
-          }}
-        >
-          <Text style={styles.buttonText}>upload</Text>
-        </TouchableOpacity>
-      </ImageBackground>
-    </View>
+    <ImageBackground style={s.background} source={bkgrndImage}>
+      <SafeAreaView style={s.safeArea}>
+        <View style={s.wrapper}>
+          <View style={s.header}>
+            <Text style={s.bigTitle}>Explore</Text>
+            <Text style={s.subTitle}>new photo communities</Text>
+          </View>
+
+          <View style={s.spacer} />
+
+          <View style={s.controls}>
+            <TouchableOpacity style={s.button}>
+              <Text style={s.title}>Log in</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
-const styles = StyleSheet.create({
-  backgroundImage: {
+const s = StyleSheet.create({
+  background: {
     width: '100%',
     height: '100%',
-    resizeMode: 'cover',
-    justifyContent: 'center',
   },
-  appButton: {
+  safeArea: {
     flex: 1,
     alignItems: 'center',
-    maxHeight: '10%',
-    margin: 10,
-    padding: 10,
   },
-  buttonText: {
-    fontSize: 30,
+  wrapper: {
+    flex: 1,
+    padding: 25,
+  },
+  header: {
+    paddingTop: 70,
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: 'Cochin-Bold',
+    color: 'white',
+  },
+  subTitle: {
+    fontSize: 20,
+    fontFamily: 'Cochin',
     color: 'lightgray',
   },
+  bigTitle: {
+    fontSize: 30,
+    fontFamily: 'Cochin',
+    color: 'white',
+  },
+  spacer: {
+    height: 470,
+  },
+  controls: {
+    flexDirection: 'row',
+  },
+  button: {
+    backgroundColor: '#333333',
+    borderRadius: 8,
+    padding: 15,
+    alignItems: 'center',
+    opacity: 0.7,
+    flex: 1,
+  },
+  gap: {
+    width: 25,
+  },
 });
+
 export default Welcome;
