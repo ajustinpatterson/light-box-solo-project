0dd558278d492782d59faedb8d7735be9de21b02
Justin Patterson <PJ943@yahoo.com>
2020-10-23 14:49:45 +0200

    refactor(client): delete unused imports from Register.js

--- client/screens/Register.js
+++ client/screens/Register.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
-  Button,
-  TouchableOpacity,
   StyleSheet,
   ImageBackground,
   StatusBar,
