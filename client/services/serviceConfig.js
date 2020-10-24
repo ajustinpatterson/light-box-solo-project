e0b85b094b2ee1e5e22e0479cc154c0fa3773d0d
Justin Patterson <PJ943@yahoo.com>
2020-10-24 09:15:47 +0200

    chore(root): clean folder to remove node_modules from future commits

--- client/services/serviceConfig.js
+++
@@ -1,7 +0,0 @@
-export default {
-  main: process.env.LOCAL_URL,
-  cloudinaryUp: process.env.CLOUDINARY_UP,
-  cloudinaryDown: process.env.CLOUDINARY_DOWN,
-  cloudinaryPreset: process.env.CLOUDINARY_PRESET,
-  unsplash: process.env.UNSPLASH_API,
-};
